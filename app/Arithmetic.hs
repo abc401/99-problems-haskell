9965b90b9a073e93d9c4aaf0272d8b31888ae486
Awab Ahmed <koibhinheen@gmail.com>
2023-08-17 22:15:57 -0700

    Problem 32

--- app/Arithmetic.hs
+++ app/Arithmetic.hs
@@ -1,18 +1,28 @@
-module Arithmatic (isPrime) where
+module Arithmetic (isPrime, Arithmetic.gcd) where
 
 ----------------------------------------------------------------------------------------------------------------------------------
-
 -- Problem 31
 -- Description:
 --    Determine whether a given integer number is prime.
 -- Solution:
+isPrime :: Integral a => a -> Bool
 isPrime 1 = False
 isPrime possiblePrime = checkDivisibility 2
   where
-    -- sqrtN = sqrt n
     checkDivisibility n
       | possiblePrime < (n * n) = True
       | possiblePrime `mod` n == 0 = False
       | otherwise = checkDivisibility $ n + 1
+
+----------------------------------------------------------------------------------------------------------------------------------
+
+-- Problem 32:
+-- Description:
+--    Determine the greatest common divisor of two positive integer numbers.
+-- Solution:
+gcd a b
+  | a < b = if mod b a == 0 then a else Arithmetic.gcd a (b - a)
+  | a > b = if mod a b == 0 then b else Arithmetic.gcd (a - b) b
+  | otherwise = a
 
 ----------------------------------------------------------------------------------------------------------------------------------
